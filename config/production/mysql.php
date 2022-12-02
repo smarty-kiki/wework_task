de20168ecd1146b717afc0eee420afa6fe80840c
yaoyang <kiki@smarty.so>
2022-12-02 19:37:28 +0800

    添加数据库配置

---
+++ config/production/mysql.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'midwares' => [
+        'migrate' => 'local',
+        'entity' => 'local',
+        'default' => 'local',
+    ],
+
+    'resources' => [
+
+        'local' => [
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'database' => 'task',
+            'username' => 'task',
+            'password' => 'password',
+
+            'read' => [
+                '/var/run/mysqld/mysqld.sock',
+            ],
+            'write' => [
+                '/var/run/mysqld/mysqld.sock',
+            ],
+            'schema' => [
+                '/var/run/mysqld/mysqld.sock',
+            ],
+
+            'options' => [
+                PDO::ATTR_CASE => PDO::CASE_NATURAL,
+                PDO::ATTR_ORACLE_NULLS => PDO::NULL_NATURAL,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_STRINGIFY_FETCHES => false,
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_PERSISTENT => false,
+            ],
+        ],
+    ],
+];
