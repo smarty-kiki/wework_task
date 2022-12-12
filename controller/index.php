0bda1e040c74807c663de9ff22ad3c0bdfdfe691
yaoyang <kiki@smarty.so>
2022-12-12 16:57:37 +0800

    debug

--- controller/index.php
+++ controller/index.php
@@ -4,7 +4,7 @@ if_get('/', function ()
 {
     $user_id = get_work_wechat_client_user();
     $config = config('work_wechat');
-    $signature_info = work_wechat_get_js_sdk_signature_info();
+    $signature_info = work_wechat_get_js_sdk_signature_info(uri());
 
     return render('index/index', [
         'user_id' => $user_id,
